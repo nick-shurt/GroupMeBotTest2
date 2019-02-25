8818ba88e33b996fbec17e4a2ffe0eb89549ede7
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-02-25 16:39:19 -0500

    Set config for groupme bot

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-exports.ACCESS_TOKEN = "GroupMe access token";
-exports.BOT_ID = "GroupMe bot ID";
-exports.GROUP_ID = "GroupMe group id";
-exports.GIHPY_KEY = 'Giphy API key';
+exports.ACCESS_TOKEN = "AH1OT9rf0u1JOH6WX8XrQ68u3tRZOh0UOm4eQ660";
+exports.BOT_ID = "cc0761c3fd53d19bc76307a6b2";
+exports.GROUP_ID = "48392578";
+exports.GIHPY_KEY = '3cNm0IxmL2htInLe6AUQBfUQ7tbYU0IC';
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 
