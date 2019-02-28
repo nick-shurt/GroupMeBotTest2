6b102b9bc89abb1f1b973579d095c52579fe8c52
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-02-28 16:22:19 -0500

    Reverted giphy.js back to original search; Created gif-mike

---
+++ src/responses/gif-mike.js
@@ -0,0 +1,18 @@
+const config = require('../config');
+const bot = require('../bot')
+const rp = require('request-promise');
+
+function trigger(msg) {
+	return /@gif-mike/i.test(msg.text);
+}
+
+async function respond(msg) {
+	try {
+		bot.postMsg('https://media.giphy.com/media/5pUGvckBvGSNvDOInk/200.gif')
+	} catch(err) {
+		console.error(err);
+	}
+}
+
+exports.trigger = trigger;
+exports.respond = respond;
