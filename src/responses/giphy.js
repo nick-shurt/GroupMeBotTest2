ff5962c42b303fbd9accf26853ef9e71efefc555
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-02-28 15:55:21 -0500

    Changed ampersand to question mark in response url parameter

--- src/responses/giphy.js
+++ src/responses/giphy.js
@@ -12,7 +12,7 @@ async function respond(msg) {
 	try {
 		let resp = await rp({
 			method: 'GET',
-			url: `http://api.giphy.com/v1/gifs/5pUGvckBvGSNvDOInk&api_key=${config.GIHPY_KEY}`,
+			url: `http://api.giphy.com/v1/gifs/5pUGvckBvGSNvDOInk?api_key=${config.GIHPY_KEY}`,
 			json: true
 		});
 		if(resp.data.length) {
