21b5e59268f6e2b82bdf60b7ea4326df37d3a9d6
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-03-02 11:44:30 -0500

    Transfered bot app to Robot Mike

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 exports.ACCESS_TOKEN = "AH1OT9rf0u1JOH6WX8XrQ68u3tRZOh0UOm4eQ660";
-exports.BOT_ID = "e70172798b3c43a5ef0292d73f";
-exports.GROUP_ID = "48392578";
-exports.GIHPY_KEY = '3cNm0IxmL2htInLe6AUQBfUQ7tbYU0IC';
+exports.BOT_ID = "d8657b5b4b6f47970f39ddaf5c";
+exports.GROUP_ID = "17201124";
+exports.GIHPY_KEY = "3cNm0IxmL2htInLe6AUQBfUQ7tbYU0IC";
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 
 exports.TEST = false;
 
-exports.USER_LIKE = { id: 'user id', name: 'user name' };
-exports.USER_ADD = { id: 'user id', name: 'user name' };
+exports.USER_LIKE = { id: "user id", name: "user name" };
+exports.USER_ADD = { id: "user id", name: "user name" };
