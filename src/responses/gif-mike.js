58a6c16613a807555fea0ad628df4244c3fe9313
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-03-02 11:17:55 -0500

    Changed trigger text for mike gif

--- src/responses/gif-mike.js
+++ src/responses/gif-mike.js
@@ -1,17 +1,17 @@
-const config = require('../config');
-const bot = require('../bot')
-const rp = require('request-promise');
+const config = require("../config");
+const bot = require("../bot");
+const rp = require("request-promise");
 
 function trigger(msg) {
-	return /mike/i.test(msg.text);
+  return /fuck you mike/i.test(msg.text);
 }
 
 async function respond(msg) {
-	try {
-		bot.postMsg('https://media.giphy.com/media/5pUGvckBvGSNvDOInk/200.gif')
-	} catch(err) {
-		console.error(err);
-	}
+  try {
+    bot.postMsg("https://media.giphy.com/media/5pUGvckBvGSNvDOInk/200.gif");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 exports.trigger = trigger;
