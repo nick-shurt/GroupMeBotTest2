3020c91ee8b5e67e765aae861529c67d473e3138
Nick Shurtleff <nshurtleff@acumensolutions.com>
2019-03-06 13:05:35 -0500

    Added timeout to gif-mike to prevent bot from posting first

--- src/responses/gif-mike.js
+++ src/responses/gif-mike.js
@@ -8,7 +8,9 @@ function trigger(msg) {
 
 async function respond(msg) {
   try {
-    bot.postMsg("https://media.giphy.com/media/5pUGvckBvGSNvDOInk/200.gif");
+    setTimeout(function() {
+      bot.postMsg("https://media.giphy.com/media/5pUGvckBvGSNvDOInk/200.gif");
+    }, 1000);
   } catch (err) {
     console.error(err);
   }
