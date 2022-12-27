115408d70a495ce38a34beff7c8f0fd82343c5c5
Nicholas Shurtleff <nicholas.shurtleff@higherechelon.com>
2022-12-27 16:28:47 -0500

    Adding user_like and user_add back into config file to fix issue

--- src/config.js
+++ src/config.js
@@ -7,3 +7,6 @@ exports.ACCESS_TOKEN_2 = "4LffyUZhNDfb3iGQAea0Mu1EvhYaFajTH5PYdqLj";
 exports.DEFAULT_MSGS_TO_ANALYZE = "10000";
 
 exports.TEST = false;
+
+exports.USER_LIKE = { id: "user id", name: "user name" };
+exports.USER_ADD = { id: "25143759", name: "YEAR OF THE BROWNS" };
