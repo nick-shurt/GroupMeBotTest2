ce12dac660bdca71f96291947016fc68b8d555e0
Nicholas Shurtleff <nicholas.shurtleff@higherechelon.com>
2022-12-29 16:16:36 -0500

    Doing some debugging idk

--- src/responses/footballOdds.js
+++ src/responses/footballOdds.js
@@ -11,7 +11,7 @@ async function respond(msg) {
     let input = msg.text.replace(/.*@odds/i, "").trim();
     let lowerInput = input.toLowerCase();
     let input_split = lowerInput.split(" ");
-    let msgToSend = '';
+    let msgToSend = 'No odds found for given input';
     var input_team;
 
     const nfl_teams = ['Cardinals',
@@ -149,23 +149,30 @@ async function respond(msg) {
                     var homePrice;
                     var awayTeam = result.away_team.toLowerCase();
                     var awayPrice;
-                    
+                    console.log('DID WE GET HERE 1');
                     if ((homeTeam.includes(input_team.toLowerCase()) || awayTeam.includes(input_team.toLowerCase())) && !found) {
+                        console.log('DID WE GET HERE 2');
                         found = true;
                         result.bookmakers.forEach((book) => {
+                            console.log('DID WE GET HERE 3');
                             if (book.key === 'fanduel') {
+                                console.log('DID WE GET HERE 4');
                                 if (book.markets[0].outcomes[0].name === result.home_team) {
+                                    console.log('DID WE GET HERE 5');
                                     homePrice = book.markets[0].outcomes[0].price;
                                     homePrice = (/^\d/.test(homePrice)) ? '+' + homePrice : homePrice;
                                 } else {
+                                    console.log('DID WE GET HERE 5');
                                     awayPrice = book.markets[0].outcomes[0].price;
                                     awayPrice = (/^\d/.test(awayPrice)) ? '+' + awayPrice : awayPrice;
                                 }
                 
                                 if (book.markets[0].outcomes[1].name === result.home_team) {
+                                    console.log('DID WE GET HERE 6');
                                     homePrice = book.markets[0].outcomes[1].price;
                                     homePrice = (/^\d/.test(homePrice)) ? '+' + homePrice : homePrice;
                                 } else {
+                                    console.log('DID WE GET HERE 6');
                                     awayPrice = book.markets[0].outcomes[1].price;
                                     awayPrice = (/^\d/.test(awayPrice)) ? '+' + awayPrice : awayPrice;
                                 }
