ee4a39cdef791d5bce10b3643ad9369b71a2fb47
Nick Shurtleff <nshurtleff15@gmail.com>
2025-07-02 13:51:33 -0400

    Fixing error and making sure update only happens if all checks pass

--- src/responses/nascar-update-team.js
+++ src/responses/nascar-update-team.js
@@ -18,10 +18,13 @@ async function respond(msg) {
     try {
         console.log('Full Message', msg);
         let message = '';
+        let update = true;
         let input = msg.text.replace(/.*@/i, "").trim();
 
+        //get team name of requestor
         let teamName = getTeamName(msg.sender_id);
 
+        //get current week race details
         const [rows] = await connection.execute(
             'SELECT name, date, number FROM `races_2025` WHERE `closed` = 0 LIMIT 1'
         );
@@ -32,45 +35,58 @@ async function respond(msg) {
         console.log('Date:', date);
         console.log('Number:', number);
 
+        //if user specified week in message, get week number and set team week; if not specified, use current week
         const weekNumber = getWeekNumber(input);
         let teamWeek = (weekNumber) ? weekNumber : number;
 
+        // extract driver names from string; if unable, input is incorrect, send message, no udpate
         const driverNames = parseSwitchRequest(input);
         if (!driverNames) {
             console.log("Invalid input format.");
             message = "Invalid input. Please check message and try again";
+            update = false;
         }
 
+        //map names to variables
         const [driverA, driverB] = driverNames.map(name => name.toLowerCase());
 
+        //get team based on user and week
         const [rows2] = await connection.execute(
             'SELECT driver1, driver2, driver3, driver4 FROM teams_2025 WHERE team_name = ? AND week = ?',
             [teamName, teamWeek]
         );
 
+        // if team not found, send message and no update
         if (rows2.length === 0) {
             console.log('Team not found.');
             message = 'Team not matched in the database';
+            update = false;
         }
 
+        //if team is found, set in variable
         const team = rows2[0];
 
+        //get the column names for the current column each driver is in
         let columnA = null, columnB = null;
         for (const [column, value] of Object.entries(team)) {
             if (value && value.toLowerCase() === driverA) columnA = column;
             if (value && value.toLowerCase() === driverB) columnB = column;
         }
 
+        // if either driver is not found on the current team, send not found message and no update
         if (!columnA || !columnB) {
             console.log('One or both drivers not found on this team.');
             message = 'One or both drivers not found on this team.';
+            update = false;
         }
 
-        const sql = `UPDATE teams_2025 SET ${columnA} = ?, ${columnB} = ? WHERE team_name = ? AND week = ?`;
-        await connection.execute(sql, [team[`${columnB}`], team[`${columnA}`], teamName, teamWeek]);
+        if (update) {
+            const sql = `UPDATE teams_2025 SET ${columnA} = ?, ${columnB} = ? WHERE team_name = ? AND week = ?`;
+            await connection.execute(sql, [team[`${columnB}`], team[`${columnA}`], teamName, teamWeek]);
 
-        console.log(`✅ Switched "${driverA}" and "${driverB}" successfully.`);
-        message = '✅  Substitution made successfully.';
+            console.log(`✅ Switched "${driverA}" and "${driverB}" successfully.`);
+            message = '✅  Substitution made successfully.';
+        }
 
         bot.postMsg(message);
     } catch (err) {
