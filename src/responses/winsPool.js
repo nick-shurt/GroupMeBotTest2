53c01ded7a534c5846dd2e8eaa28403684458c3b
Nick Shurtleff <nshurtleff15@gmail.com>
2025-12-02 15:49:08 -0500

    More testing for wins pool update

--- src/responses/winsPool.js
+++ src/responses/winsPool.js
@@ -6,8 +6,76 @@ function trigger(msg) {
 
 async function respond(msg) {
   try {
+    const pool = {
+      pool: [
+        {
+          nick: {
+            teams: { team1: "bills", team2: "steelers", team3: "colts" },
+            wins: 0
+          }
+        },
+        {
+          matt: {
+            teams: { team1: "49ers", team2: "vikings", team3: "falcons" },
+            wins: 0
+          }
+        },
+        {
+          dockman: {
+            teams: { team1: "buccaneers", team2: "cowboys", team3: "jaguars" },
+            wins: 0
+          }
+        },
+        {
+          donna: {
+            teams: { team1: "lions", team2: "seahawks", team3: "raiders" },
+            wins: 0
+          }
+        },
+        {
+          jim: {
+            teams: { team1: "commanders", team2: "broncos", team3: "bears" },
+            wins: 0
+          }
+        },
+        {
+          joey: {
+            teams: { team1: "ravens", team2: "patriots", team3: "panthers" },
+            wins: 0
+          }
+        },
+        {
+          john: {
+            teams: { team1: "packers", team2: "bengals", team3: "jets" },
+            wins: 0
+          }
+        },
+        {
+          mike: {
+            teams: { team1: "eagles", team2: "chargers", team3: "titans" },
+            wins: 0
+          }
+        },
+        {
+          pat: {
+            teams: { team1: "rams", team2: "cardinals", team3: "dolphins" },
+            wins: 0
+          }
+        },
+        {
+          ricky: {
+            teams: { team1: "chiefs", team2: "texans", team3: "giants" },
+            wins: 0
+          }
+        }
+      ]
+    };
     fetchNflWins()
-      .then(wins => console.log("NFL Wins:", wins))
+      .then((winsByTeam) => {
+        console.log("NFL Wins:", winsByTeam);
+        const updated = updatePoolWins(pool, winsByTeam);
+        console.log(JSON.stringify(updated, null, 2));
+      })
       .catch(err => console.error(err));
   } catch (err) {
     console.error(err);
@@ -57,6 +125,29 @@ async function fetchNflWins() {
   }
 
   return winsByTeam;
+}
+
+function updatePoolWins(poolData, winsByTeam) {
+  poolData.pool.forEach(entry => {
+    const playerName = Object.keys(entry)[0];
+    const playerData = entry[playerName];
+
+    const teams = playerData.teams;
+
+    let totalWins = 0;
+
+    // Loop through team1, team2, team3
+    Object.values(teams).forEach(teamName => {
+      const abbr = teamName.toUpperCase();   // your teams are lowercase strings like "bills"
+      if (winsByTeam[abbr] !== undefined) {
+        totalWins += winsByTeam[abbr];
+      }
+    });
+
+    playerData.wins = totalWins;
+  });
+
+  return poolData;
 }
 
 exports.trigger = trigger;
